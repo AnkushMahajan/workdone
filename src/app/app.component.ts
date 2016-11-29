0a93766b0e28eefc6c716f1f863f170429ae89ec
Ankush Mahajan <vish_sukhoi@yahoo.co.in>
2016-11-29 13:34:54 +1100

    Added TS Firebase dependency
    Firebase can / should be configured from the app.component.ts instead
    of HTML. Added the rollup config for accessing the firebase common JS
    module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import firebase from 'firebase';
 
 
 @Component({
@@ -12,6 +13,17 @@ export class MyApp {
   rootPage = HomePage;
 
   constructor(platform: Platform) {
+
+    // Initialize Firebase
+    let config = {
+      apiKey: "AIzaSyDscYaVRH0tXCBc0efNfqie4-bQ_QeKkME",
+      authDomain: "workdone-7cee3.firebaseapp.com",
+      databaseURL: "https://workdone-7cee3.firebaseio.com",
+      storageBucket: "workdone-7cee3.appspot.com",
+      messagingSenderId: "519784053906"
+    }
+    console.log(firebase);
+    firebase.initializeApp(config);
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
